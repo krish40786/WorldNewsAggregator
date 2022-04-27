2bfa2dcae72a47545b24e403697b371c3bab8bd5
Alan Wei <aywei@uwaterloo.ca>
2022-04-27 11:06:18 -0400

    Added functionality for creating the rss url based on inputted keyword, country and language, and fetching the data from this url.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import React from 'react';
 
-function App() {
+const App = (props) => {
+  const [keyword, setKeyword] = useState('Ukraine war');
+  const [country, setCountry] = useState('CA');
+  const [language, setLanguage] = useState('en');
+
+  var RSS_URL = `http://localhost:8010/proxy/rss/search?q=`;
+
+  const search = (event) => {
+    event.preventDefault();
+    console.log('button clicked', event.target);
+    RSS_URL += keyword + '&hl=' + language + '&gl=' + country + '&ceid=' + country + ':' + language;
+    console.log("RSS_URL:", RSS_URL);
+    fetch(RSS_URL, {mode: 'cors'})  // axios might be better than fetch
+    .then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(data => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
+  const handleKeywordChange = (event) => {
+    //console.log(event.target.value);
+    setKeyword(event.target.value);
+  }
+
+  const handleCountryChange = (event) => {
+    //console.log(event.target.value);
+    setCountry(event.target.value);
+  }
+
+  const handleLanguageChange = (event) => {
+    //console.log(event.target.value);
+    setLanguage(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>World News Aggregator</h1>
+      <form onSubmit={search}>
+        keyword
+        <input 
+          value={keyword} 
+          onChange={handleKeywordChange}
+        />
+        geoLocation
+        <input 
+          value={country} 
+          onChange={handleCountryChange}
+        />
+        language
+        <input 
+          value={language} 
+          onChange={handleLanguageChange}
+        />
+        <button type="submit">search</button>
+      </form>
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App 
